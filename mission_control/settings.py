59df2cd8135d219757c6a19f6eb4b77b5fd34259
James Scott <james@247-data.com>
2026-04-24 23:01:07 +0000

    database connection debugging

--- mission_control/settings.py
+++ mission_control/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import os
 import environ
 
+from dotenv import load_dotenv
+
+# This loads the variables from your .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,12 +89,18 @@ WSGI_APPLICATION = "mission_control.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'eclipse_mission_control'),
+        'USER': os.getenv('DB_USER', 'eclipse_user'), # Use 'root' or whatever is in your .env
+        'PASSWORD': os.getenv('DB_PASSWORD', 'iGCnGKM27VL2bdGaT8W9SiqC6VmSsjn8kHrhD3E07XQLJCqeC1'), # It will look for the KEY 'DB_PASSWORD'
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
