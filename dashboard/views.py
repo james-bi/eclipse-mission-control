d2f18522c59a77da999df723e59298143efc4c7d
James Scott <34707842+james-bi@users.noreply.github.com>
2026-04-24 23:15:10 +0000

    fixing hash issue

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Balloon
 
-# Create your views here.
+def dashboard_home(request):
+    balloons = Balloon.objects.all()
+    context = {
+        'balloons': balloons
+    }
+    return render(request, 'dashboard/index.html', context)
