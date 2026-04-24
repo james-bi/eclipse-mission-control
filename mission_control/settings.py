8d311a80b07aa72562fdce54e953dcc61b90fd66
James Scott <james@247-data.com>
2026-04-24 16:57:12 +0000

    setting up wsgi

--- mission_control/settings.py
+++ mission_control/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wl+04k9^o919wvgz0shg2sf99++)+_#zt_p)v5bi44yjvhft_y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['mission-control.247-data.com', '63.33.141.98', 'localhost']
 
 # Application definition
 
@@ -116,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
